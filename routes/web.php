39d961c52c9b065e4cd24476c3079b82b2aa9711
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-15 10:10:07 +0330

    Controller Route Groups! Amazing!

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(PostController::class)->group(function (){
+    Route::get('/posts','index');
+    Route::get('/posts/{posts}','show');
+    Route::post('/posts','store');
+
+});
+
+
